4242a1b44056fa22612270f7a2f57d0fcbfa5315
Ben Ong <ben_ong@yahoo.com>
2021-05-27 16:49:38 -0700

    Added initial webcam project

--- webcam_fun/scripts.js
+++ webcam_fun/scripts.js
@@ -3,3 +3,50 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    //--- Video Setup ---
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            //video.src = window.URL.createObjectURL(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.log(`Error: Webcam access may have been denied`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    //console.log(width, height);
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    //-- Play sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //-- Take data out of canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    //link.textContent = 'Download image';
+    link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+
+//----- Event Listeners -----
+video.addEventListener('canplay', paintToCanvas);
